03b2ccd47d7ad8e3ee7104b113b145a5d06d8161
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2019-04-19 21:04:25 +0200

    Add solution-folder automation

---
+++ ProjectsInSolutionFolder/ProjectsInSolutionFolder/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using EnvDTE;
+using EnvDTE80;
+
+namespace SolutionAutomation
+{
+    class Program
+    {
+        private const string VisualStudio2019 = "VisualStudio.DTE.16.0";
+
+        static void Main(string[] args)
+        {
+            var dteType = Type.GetTypeFromProgID(VisualStudio2019, true);
+            var dte = (DTE)Activator.CreateInstance(dteType);
+
+            var sln = (SolutionClass)dte.Solution;
+            sln.Open(@"C:\Home\Projects\RoboNuGet\RoboNuGet.sln");
+
+            var lib = FindSolutionFolder(sln, "lib");
+
+            var projectPaths = new[]
+            {
+                @"C:\Home\Projects\Reusable\Reusable.Core\Reusable.Core.csproj",
+                @"C:\Home\Projects\Reusable\Reusable.Flexo\Reusable.Flexo.csproj",
+                @"C:\Home\Projects\Reusable\Reusable.IOnymous\Reusable.IOnymous.csproj"
+            };
+
+            foreach (var path in projectPaths)
+            {
+                var name = Path.GetFileNameWithoutExtension(path);
+                if (!lib.Parent.ProjectItems.Cast<ProjectItem>().Any(pi => pi.Name == name))
+                {
+                    lib.AddFromFile(path);
+                    Console.WriteLine($"Added: {name}");
+                }
+            }
+
+            dte.Solution.Close(true);
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+
+        private static SolutionFolder FindSolutionFolder(SolutionClass sln, string folderName)
+        {
+            var solutionFolder =
+                sln.Projects
+                    .OfType<Project>()
+                    .FirstOrDefault(p => p.Name.Equals(folderName, StringComparison.OrdinalIgnoreCase))
+                    ?.Object as SolutionFolder;
+
+            if (solutionFolder is null)
+            {
+                solutionFolder = (sln as Solution2).AddSolutionFolder(folderName).Object as SolutionFolder;
+            }
+
+            return solutionFolder;
+        }
+    }
+}
